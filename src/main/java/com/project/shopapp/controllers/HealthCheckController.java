16a491c06d7c67c7c63443627c8695a01b364eaf
Hoàng <luutru040204@gmail.com>
2025-05-28 21:56:58 +0700

    add kafka

--- src/main/java/com/project/shopapp/controllers/HealthCheckController.java
+++ src/main/java/com/project/shopapp/controllers/HealthCheckController.java
@@ -1,17 +1,15 @@
 package com.project.shopapp.controllers;
 
 import com.project.shopapp.models.Category;
-import com.project.shopapp.services.CategoryService;
+import com.project.shopapp.services.category.CategoryService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 @RestController
 @RequestMapping("${api.prefix}/healthcheck")
@@ -24,7 +22,7 @@ public class HealthCheckController {
         try {
             List<Category> categories = categoryService.getAllCategories();
             // Get the computer name
-            String computerName = InetAddress.getLocalHost().getHostName();
+            String computerName = InetAddress.getLocalHost().getHostName();        
             return ResponseEntity.ok("ok, Computer Name: " + computerName);            
         }catch (Exception e) {
             return ResponseEntity.badRequest().body("failed");
